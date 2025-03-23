6d9a6371fa0218c9567c4c81252bca5b31c1708f
A1gor <fordzio222@gmail.com>
2025-03-23 11:29:36 +0100

    Wprowadzenie poprawek do commitów 6 i 7

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,7 +4,6 @@ public class DefaultCountingOutRhymer {
 
     public static final int CAPACITY = 12;
     public static final int EMPTY_INDEX = -1;
-    public static final int FULL_INDEX = CAPACITY - 1;
     public static final int ERROR_VALUE = -1;
 
     private final int[] numbers = new int[CAPACITY];
@@ -20,7 +19,7 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean isFull() {
-        return total == FULL_INDEX;
+        return total == CAPACITY - 1;
     }
 
     protected int peekaboo() {
