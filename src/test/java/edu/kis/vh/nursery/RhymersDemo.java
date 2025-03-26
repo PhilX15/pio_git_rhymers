4bc7cd1041f18eb1f7eb547b57ae54745629be44
FKalin15 <filipkal55@gmail.com>
2025-03-26 13:48:56 +0100

    13. Rozbicie metody main w RhymersDemo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -10,8 +10,12 @@ class RhymersDemo {
     public static void main(String[] args) {
         RhymersFactory factory = new DefaultRhymersFactory();
 
+        testRhymers(factory);
+    }
+
+    private static void testRhymers(RhymersFactory factory) {
         DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
-                factory.getFIFORhymer(), factory.getHanoiRhymer()};
+                factory.getFIFORhymer(), factory.getHanoiRhymer() };
 
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
@@ -27,9 +31,7 @@ class RhymersDemo {
             System.out.println();
         }
 
-        System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
-
+        System.out.println("total rejected is " + ((HanoiRhymer) rhymers[3]).reportRejected());
     }
 
 }
